8c06f175f967f6df1fdd92bd87ba766a6adeaf3e
Justin <justinjoseph1@gmail.com>
2014-04-08 09:10:41 -0500

    Added Sinatra app, included Rack::Test and passed test

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,2 +1,17 @@
 require_relative '../parser.rb'
 require 'rack/test'
+
+set :environment, :test
+
+def app
+	Sinatra::Application
+end
+
+describe 'Parser Service' do
+	include Rack::Test::Methods
+
+	it "should load the home page" do
+		get '/'
+		last_response.should be_ok
+	end
+end

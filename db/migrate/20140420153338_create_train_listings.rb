583059d856c17d11cdb367a4daf3acfb9ecef002
jjoseph05 <justinjoseph1@gmail.com>
2014-04-20 15:51:33 -0500

    Added train_line, route_name, run_number, and operaator_id attributes

--- db/migrate/20140420153338_create_train_listings.rb
+++ db/migrate/20140420153338_create_train_listings.rb
@@ -1,4 +1,12 @@
 class CreateTrainListings < ActiveRecord::Migration
   def change
+  	create_table :train_listings do |t|
+  		t.string :train_line
+  		t.string :route_name
+  		t.string :run_number
+  		t.string :operator_id
+  		
+  		t.timestamps
+  	end
   end
 end

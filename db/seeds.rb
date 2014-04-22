7e0e1e100ce8a83845dc150a37157fa54cf01a3c
jjoseph05 <justinjoseph1@gmail.com>
2014-04-22 01:43:04 -0500

    Updated seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,12 @@ require 'csv'
 TrainSchedule.delete_all
 TrainListing.delete_all
 
-schedule = TrainSchedule.new
+@schedule = TrainSchedule.create
 
 CSV.foreach('./trainschedule.csv', {:header_converters => :downcase, :headers => true}) do |row|
-	schedule << TrainListing.create( row['train_line'], row['route_name'], row['run_number'], row['operator_id'] )
-	  User.create :first_name => Faker::Name.first_name,
-              :last_name  => Faker::Name.last_name,
-              :email      => Faker::Internet.email,
+	@schedule << TrainListing.create(train_line: row['train_line'], 
+																	route_name: row['route_name'], 
+																	run_number: row['run_number'], 
+																	operator_id: row['operator_id'],
+																	train_schedule_id: TrainSchedule.last.id)
 end

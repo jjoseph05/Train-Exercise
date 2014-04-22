4b17530efb85d9b84c6d17a622507f04ccbe935b
jjoseph05 <justinjoseph1@gmail.com>
2014-04-22 01:56:33 -0500

    Updated issue with seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@ TrainSchedule.delete_all
 TrainListing.delete_all
 
 @schedule = TrainSchedule.create
-
+@schedule.schedule = []
 CSV.foreach('./trainschedule.csv', {:header_converters => :downcase, :headers => true}) do |row|
-	@schedule << TrainListing.create(train_line: row['train_line'], 
-																	route_name: row['route_name'], 
-																	run_number: row['run_number'], 
-																	operator_id: row['operator_id'],
-																	train_schedule_id: TrainSchedule.last.id)
+	@schedule.schedule << TrainListing.create(train_line: row['train_line'], 
+																						route_name: row['route_name'], 
+																						run_number: row['run_number'], 
+																						operator_id: row['operator_id'],
+																						train_schedule_id: TrainSchedule.last.id)
 end

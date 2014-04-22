df42eee10428360ff78124b180db47f429e86076
jjoseph05 <justinjoseph1@gmail.com>
2014-04-22 12:00:04 -0500

    Added import method

--- app/models/train_schedule.rb
+++ app/models/train_schedule.rb
@@ -1,3 +1,14 @@
 class TrainSchedule < ActiveRecord::Base
 	has_many :train_listings
+
+	def import(file)
+		CSV.foreach(file, {:header_converters => :downcase, :headers => true}) do |row|
+		  @schedules.schedule << TrainListing.create(train_line: row['train_line'], 
+																						route_name: row['route_name'], 
+																						run_number: row['run_number'], 
+																						operator_id: row['operator_id'],
+																						train_schedule_id: TrainSchedule.last.id)
+    end
+	end
+
 end

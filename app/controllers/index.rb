b7c04860d97f422d18fd51f9d0f3370cbffd5794
jjoseph05 <justinjoseph1@gmail.com>
2014-04-22 10:58:55 -0500

    Added upload post route to index controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,3 +8,12 @@ get '/upload' do
   haml :index
 end
 
+post '/upload' do
+  unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+    return haml(:index)
+  end
+  while blk = tmpfile.read(65536)
+      File.open(File.join(Dir.pwd,"public/uploads", name), "wb") { |f| f.write(tmpfile.read) }
+  end
+ 'success'
+end

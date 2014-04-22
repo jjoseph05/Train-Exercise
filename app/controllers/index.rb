5e8593abab2eab5f3efa861f0e863630fded2244
jjoseph05 <justinjoseph1@gmail.com>
2014-04-22 12:00:53 -0500

    Updated upload route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,12 +8,18 @@ get '/upload' do
   haml :index
 end
 
+# post '/upload' do
+#   unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+#     return haml(:index)
+#   end
+#   while blk = tmpfile.read(65536)
+#       File.open(File.join(Dir.pwd,"public/uploads", name), "wb") { |f| f.write(tmpfile.read) }
+#   end
+#  'success'
+# end
+
 post '/upload' do
-  unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
-    return haml(:index)
-  end
-  while blk = tmpfile.read(65536)
-      File.open(File.join(Dir.pwd,"public/uploads", name), "wb") { |f| f.write(tmpfile.read) }
-  end
- 'success'
+	@schedules = TrainSchedule.create
+	@schedules.schedule = []
+  @schedules.import(params[:file])
 end

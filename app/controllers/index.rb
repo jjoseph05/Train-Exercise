95be83dbe18aaa6fd0031a5bb06c8ba31afdcf72
jjoseph05 <justinjoseph1@gmail.com>
2014-04-22 22:03:54 -0500

    Fixed issue with upload

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,21 +8,37 @@ get '/upload' do
   haml :index
 end
 
-# post '/upload' do
-#   unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
-#     return haml(:index)
-#   end
-#   while blk = tmpfile.read(65536)
-#       File.open(File.join(Dir.pwd,"public/uploads", name), "wb") { |f| f.write(tmpfile.read) }
-#   end
-#  'success'
-# end
-
 post '/upload' do
-	# @schedule = TrainSchedule.create
-	# @schedule.schedule = []
-	puts "*=="*80
-	
-  TrainSchedule.import(params[:filename])
-  puts "*=="*80
+	@schedule = TrainSchedule.create
+	@schedule.schedule = []
+  unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+    return haml(:index)
+  end
+  while blk = tmpfile.read(65536)
+  	CSV.foreach(params['file'][:tempfile], {:header_converters => :downcase, :headers => true}) do |row|
+  		row.to_a
+  		puts "*=="*80
+  		@schedule.schedule = [] << TrainListing.create(train_line: row['train_line'], 
+																								route_name: row['route_name'], 
+																								run_number: row['run_number'], 
+																								operator_id: row['operator_id'],
+																								train_schedule_id: TrainSchedule.last.id)
+  	end
+  	# TrainSchedule.import(tmpfile)
+      # File.open(File.join(Dir.pwd,"public/uploads", name), "wb") { |f| f.write(tmpfile.read) }
+  end
+ 'success'
+
+redirect '/'
 end
+
+# post '/upload' do
+# 	# @schedule = TrainSchedule.create
+# 	# @schedule.schedule = []
+# 	puts "*=="*80
+# 	p file_data = params[:file][:tempfile].read
+#   TrainSchedule.import(file_data)
+#   puts "*=="*80
+
+#   haml :index
+# end
--- app/models/train_schedule.rb
+++ app/models/train_schedule.rb
@@ -2,12 +2,13 @@ class TrainSchedule < ActiveRecord::Base
 	has_many :train_listings
 
 	def self.import(file)
-		CSV.foreach(file, {:header_converters => :downcase, :headers => true}) do |row|
+		CSV.foreach(file, {:header_converters => [:downcase,:symbol], :headers => true}) do |row|
+
 		  @schedule.schedule << TrainListing.create(train_line: row['train_line'], 
-																						route_name: row['route_name'], 
-																						run_number: row['run_number'], 
-																						operator_id: row['operator_id'],
-																						train_schedule_id: TrainSchedule.last.id)
+																								route_name: row['route_name'], 
+																								run_number: row['run_number'], 
+																								operator_id: row['operator_id'],
+																								train_schedule_id: TrainSchedule.last.id)
     end
 	end
 

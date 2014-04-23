3ca5ae6e0d659ec4adc56cb2a384486c953d41f8
jjoseph05 <justinjoseph1@gmail.com>
2014-04-23 16:27:03 -0500

    Removed duplicate code

--- app/models/train_schedule.rb
+++ app/models/train_schedule.rb
@@ -1,15 +1,3 @@
 class TrainSchedule < ActiveRecord::Base
 	has_many :train_listings
-
-	def self.import(file)
-		CSV.foreach(file, {:header_converters => [:downcase,:symbol], :headers => true}) do |row|
-
-		  @schedule.schedule << TrainListing.create(train_line: row['train_line'], 
-																								route_name: row['route_name'], 
-																								run_number: row['run_number'], 
-																								operator_id: row['operator_id'],
-																								train_schedule_id: TrainSchedule.last.id)
-    end
-	end
-
 end

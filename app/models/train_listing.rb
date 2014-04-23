d1e954ef269d944aa13aee10ec4700793d5e85c2
jjoseph05 <justinjoseph1@gmail.com>
2014-04-23 07:22:28 -0500

    Added run number uniqueness validation

--- app/models/train_listing.rb
+++ app/models/train_listing.rb
@@ -2,4 +2,5 @@ class TrainListing < ActiveRecord::Base
 	belongs_to :train_schedule
 
 	validates :train_line, :route_name, :run_number, :operator_id, presence: true
+	validates_uniqueness_of :run_number
 end
